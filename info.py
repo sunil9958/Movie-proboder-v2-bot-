e2d4ba1e2252e3f49b6eac7f999925a1e4b74167
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-03-19 09:21:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,17 +21,17 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/65fe86fc02a73f6fcf0ce.jpg https://telegra.ph/file/6fa70325813885809a64a.jpg https://telegra.ph/file/e06afc1e7abbcd8d4213a.jpg https://telegra.ph/file/3f4040b320d9b7840200a.jpg https://telegra.ph/file/3950fad740fb8ea894df7.jpg')).split() #SAMPLE PIC
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/86b7b7e2aa7e38f328902.jpg")
+PICS = (environ.get('PICS', 'https://telegra.ph/file/a41bd7562e98b267d601e.jpg https://telegra.ph/file/20280cba4d170583e699f.jpg https://telegra.ph/file/c7e58ea62c20738cf03a9.jpg')).split() #SAMPLE PIC
+NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf2172https://telegra.ph/file/85d361ab4cb6511006022.mp452964202.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/c7e58ea62c20738cf03a9.mp4")
+SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/9333b52c49cc772dee032.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8169830d821.jpg'))
-CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
+CODE = (environ.get('CODE', 'https://telegra.ph/file/e990dd80ce033f78f48c8.jpg'))
 
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
-STREAM_API = (environ.get('STREAM_API', 'ae0832f39e24094a0661626a792e6a2b8880e310'))
-STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/HowToOpenLinkHP/69'))
+STREAM_API = (environ.get('STREAM_API', '879753c6ad56b7b44a450f3bb0279c22f34360c4'))
+STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/+RK-uQG5u-4RhMzE1'))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
